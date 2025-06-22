0480a18be0e98199fac92f74121c18e747771cdc
AhmedTalbii <obetox1@gmail.com>
2025-06-22 10:19:08 +0100

    final

--- main/main.go
+++ main/main.go
@@ -11,28 +11,32 @@ import (
 func main() {
 	args := os.Args[1:]
 	if len(args) != 2 || strings.HasSuffix(args[1], ".go") || !strings.HasSuffix(args[1], ".txt") {
+		fmt.Println("Invalid Input.")
 		return
 	}
 	file, errOs := os.Open(args[0])
 	if errOs != nil {
+		fmt.Println("Error in opening file :",errOs)
 		return
 	}
 	defer file.Close()
 	data, errIo := io.ReadAll(file)
 	if errIo != nil {
+		fmt.Println("Error in read the file :",errOs)
 		return
 	}
 	str := string(data)
 	strL := goreloded.Solve(str)
 	res , errRs := os.Create(args[1])
 	if errRs != nil {
+		fmt.Println("Error in creating file :",errOs)
 		return
 	}
 	defer res.Close()
 	
 	_ , errRf := res.WriteString(strL)
 	if errRf != nil {
+		fmt.Println("Error in Writing in the file :",errRf)
 		return
 	}
-	fmt.Println(strL)
 }
